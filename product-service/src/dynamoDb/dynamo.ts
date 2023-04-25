ed707876dc94fd94108c99f8cabaf27cb041bbe1
Stanislau Ukolau <Stanislau_Ukolau@epam.com>
2023-04-25 19:26:28 +0300

    Refactoring

--- product-service/src/dynamoDb/dynamo.ts
+++ product-service/src/dynamoDb/dynamo.ts
@@ -2,10 +2,6 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { SSM } from '@aws-sdk/client-ssm';
 import {
   DynamoDBDocumentClient,
-  ScanCommand,
-  PutCommand,
-  GetCommand,
-  DeleteCommand,
 } from "@aws-sdk/lib-dynamodb";
 
 const ssm = new SSM({region: 'us-east-1'});
@@ -15,9 +11,15 @@ export const ddbClient = new DynamoDBClient({region: 'us-east-1'});
 export const ddbDocumentClient = DynamoDBDocumentClient.from(ddbClient);
 
 export async function getSSMParameter(name: string): Promise<string> {
-  const parameter = await ssm.getParameter({
-    Name: name,
-  });
+  try {
+    const parameter = await ssm.getParameter({
+      Name: name,
+    });
 
-  return parameter.Parameter.Value;
+    return parameter.Parameter.Value;
+
+  } catch (e) {
+    return e;
+  }
 }
+
